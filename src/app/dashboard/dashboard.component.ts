944a965a15c9f883d04fe224f93b4e350406e4e8
Davidliu11 <david.liu@clarifyhealth.com>
2019-11-26 00:48:30 +0800

    Added the test logic

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 interface Tester {
     name: string;
     rightAnswer: number;
     wrongAnswer: number;
-    failed: boolean;
+    passed: boolean;
 }
 declare var require: any;
 @Component({
@@ -16,7 +17,7 @@ export class DashboardComponent implements OnInit {
     sideMenuOpened = true;
     currentTest: string;
     allWords = require('./words.json');
-    currentWord = this.allWords[0];
+    currentWordIndex = 0;
 
     currentTester: Tester;
     onlineMembers: Tester[] = [
@@ -24,69 +25,72 @@ export class DashboardComponent implements OnInit {
             name: 'Amy',
             rightAnswer: 0,
             wrongAnswer: 0,
-            failed: false
+            passed: false
         },
         {
             name: 'Charles',
             rightAnswer: 0,
             wrongAnswer: 0,
-            failed: false
+            passed: false
         },
         {
             name: 'David',
             rightAnswer: 0,
             wrongAnswer: 0,
-            failed: false
+            passed: false
         },
         {
             name: 'Vivian',
             rightAnswer: 0,
             wrongAnswer: 0,
-            failed: false
+            passed: false
         },
         {
             name: 'Jack',
             rightAnswer: 0,
             wrongAnswer: 0,
-            failed: false
+            passed: false
         },
         {
             name: 'Novia',
             rightAnswer: 0,
             wrongAnswer: 0,
-            failed: false
+            passed: false
         },
         {
             name: 'HanLi',
             rightAnswer: 0,
             wrongAnswer: 0,
-            failed: false
+            passed: false
         },
         {
             name: 'Sander',
             rightAnswer: 0,
             wrongAnswer: 0,
-            failed: false
+            passed: false
         },
         {
             name: 'TaoLue',
             rightAnswer: 0,
             wrongAnswer: 0,
-            failed: false
+            passed: false
         },
         {
             name: 'Joey',
             rightAnswer: 0,
             wrongAnswer: 0,
-            failed: false
+            passed: false
         },
         {
             name: 'Ginny',
             rightAnswer: 0,
             wrongAnswer: 0,
-            failed: false
+            passed: false
         }
     ];
+
+    testerLastStatus = true;
+    testStarted = false;
 
     constructor() {
     }
@@ -94,6 +98,65 @@ export class DashboardComponent implements OnInit {
     ngOnInit() {}
 
     selectMember(member: Tester) {
-        this.currentTester = member;
+        if (member === this.currentTester) {
+            this.currentTester = undefined;
+            if (!member.passed) {
+                this.resetTestStatus(member);
+            }
+            return;
+        }
+        this.currentTester = this.resetTestStatus(member);
+    }
+
+    resetTestStatus(member: Tester) {
+        member.passed = false;
+        member.rightAnswer = 0;
+        member.wrongAnswer = 0;
+        this.testerLastStatus = true;
+        this.testStarted = false;
+        return member;
+    }
+
+    toggleNav() {
+        this.sideMenuOpened = !this.sideMenuOpened;
+    }
+
+    rightAnswer() {
+        this.testerLastStatus = true;
+        this.currentTester.rightAnswer += 1;
+        if (this.currentTester.rightAnswer === 3) {
+            this.currentTester.passed = true;
+            this.currentTester = undefined;
+            Swal.fire('Confrats!', 'You passed this test!', 'success');
+        }
+        this.nextWords();
+    }
+
+    wrongAnswer() {
+        this.currentTester.wrongAnswer += 1;
+        if (this.testerLastStatus === false) {
+            this.currentTester = undefined;
+            Swal.fire('Failed', 'You failed this test.', 'error');
+        } else {
+            this.testerLastStatus = false;
+        }
+
+        this.nextWords();
+    }
+
+    nextWords() {
+        this.currentWordIndex = this.currentWordIndex + 2 > this.allWords.length ? 0 : this.currentWordIndex + 1;
+    }
+
+    startTest() {
+        this.testStarted = true;
+    }
+
+    getClass(member: Tester) {
+        return {
+            'highlighted-menu-item': member === this.currentTester,
+            'passed-effect': member.passed,
+            'failed-effect ': !member.passed && member.wrongAnswer > 0
+        }
     }
 }
